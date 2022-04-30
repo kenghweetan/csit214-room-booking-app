565aed52e218dbddf3cb53eaa9dd05408b6ce38b
thzx66 <103661050+thzx66@users.noreply.github.com>
2022-04-30 14:56:47 +0800

    Finished Overall Layout

--- public/javascript/calendar-view.js
+++ public/javascript/calendar-view.js
@@ -1,11 +1,33 @@
-window.onload = createWeekView;
+window.onload = loadDefaultView;
+
+function loadDefaultView() {
+  document.getElementById("sDate").valueAsDate = new Date();
+  createWeekView();
+}
 
 function createWeekView() {
-  let date = new Date();
+  // let date = new Date();
+  let date = document.getElementById("sDate").valueAsDate;
+  // console.log(date.getDate());
+  // console.log(new Date());
   const week = document.getElementById("week");
+  let newWeek = [];
+
+  // week.innerHTML = null; (null/empty string)
+  // while (week.firstChild) {
+  //   week.removeChild(week.lastChild)
+  // }
+
   for (let i = 0; i < 6; i++) {
-    week.appendChild(createTimeLineCard(date.setDate(date.getDate() + 1)));
+    // week.appendChild(createTimeLineCard(date.setDate(date.getDate() + 1)));
+    newWeek.push(
+      createTimeLineCard(date.setDate(date.getDate() + (i === 0 ? 0 : 1)))
+    );
   }
+
+  console.log(newWeek);
+  console.log(...newWeek);
+  week.replaceChildren(...newWeek);
 }
 
 function createTimeLineCard(date) {
@@ -23,6 +45,7 @@ function createTimeLineTable(date) {
   timeLineTable.setAttribute("class", "table");
   timeLineTable.setAttribute("id", "tableid");
   const caption = document.createElement("caption");
+  caption.setAttribute("id", "captionDate");
 
   caption.innerHTML = new Date(date).toString();
 
@@ -38,9 +61,11 @@ function createTLHead() {
 
   const tr = document.createElement("tr");
   thead.appendChild(tr);
+
   const roomsCol = document.createElement("th");
-  roomsCol.innerHTML = "rooms";
+  roomsCol.innerHTML = "Rooms";
   tr.appendChild(roomsCol);
+
   for (i = 9; i <= 18; i++) {
     const th = document.createElement("th");
     th.innerHTML = i + ":00";
@@ -61,8 +86,13 @@ function createTLBody() {
 
     for (i = 9; i <= 18; i++) {
       const td = document.createElement("td");
+      td.setAttribute("class", "selectable");
+      // td.addEventListener("click", function(event){event.preventDefault(); console.log("testclick")})
       if (Math.random() < 0.5) {
         td.setAttribute("style", "background-color: blue;");
+        td.setAttribute("class", "booked");
+      } else {
+        td.setAttribute("class", "selectable");
       }
 
       tr.appendChild(td);
