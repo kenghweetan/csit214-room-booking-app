baa3379b19e77d08b886d316f46f700f0c049ddb
Tan Keng Hwee <tankenghwee1996@gmail.com>
2022-05-02 21:05:21 +0800

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -2,26 +2,28 @@ const db = require("../db/models/index");
 
 module.exports = async function login(req, res) {
   const { userType, email, password } = req.body;
-  const userModel = db[userType];
+  const userTypeModel = db[userType];
 
   if (!(email && password))
-    return res.status(401).send("Please enter Email and Password");
+    return res.status(401).send("Please enter Email or Password");
 
-  if (!userModel) return res.status(401).send(`Invalid User Type: ${userType}`);
+  if (!userTypeModel)
+    return res.status(401).send(`Invalid User Type: ${userType}`);
 
-  // Execute SQL query that'll select the account from the database based on the specified username and password
-  const validStudent = await userModel.findOne({
+  // Execute SQL query that'll select the account from the database based on the specified email and password
+  const validStudent = await userTypeModel.findOne({
     where: { email: email, password: password },
   });
+
   console.log(validStudent);
-  if (validStudent) {
-    // Authenticate the user
-    req.session.loggedIn = true;
-    req.session.email = email;
-    req.session.userType = userType;
-    // Redirect to home page
-    return res.status(200).end();
-  } else {
+
+  if (!validStudent)
     return res.status(401).send("Incorrect Email and/or Password");
-  }
+
+  // Authenticate the user
+  req.session.loggedIn = true;
+  req.session.email = email;
+  req.session.userType = userType;
+
+  return res.status(200).end();
 };
