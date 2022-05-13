bcb4ad69b5d0174fa3190441322a1144d3572ccb
zerminchu <44081923+zerminchu@users.noreply.github.com>
2022-05-13 23:15:17 +0800

    1

--- db/migrations/06-create-booking.js
+++ db/migrations/06-create-booking.js
@@ -1,61 +1,61 @@
 "use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("bookings", {
-      bookingId: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-      },
-      status: {
-        type: Sequelize.ENUM("confirmed", "canceled"),
-        allowNull: false,
-      },
-      startDateTime: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      endDateTime: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      grossPrice: {
-        type: Sequelize.DECIMAL(15, 2),
-        allowNull: false,
-      },
-      netPrice: {
-        type: Sequelize.DECIMAL(15, 2),
-        allowNull: false,
-      },
-      PromoCodeName: {
-        type: Sequelize.STRING,
-        references: {
-          model: "PromoCodes",
-          key: "name",
-        },
-      },
-      RoomName: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        onDelete: "CASCADE",
-        references: {
-          model: "Rooms",
-          key: "name",
-        },
-      },
-      StudentEmail: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        references: {
-          model: "students",
-          key: "email",
-        },
-      },
-    });
-  },
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable("bookings", {
+            bookingId: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true,
+            },
+            status: {
+                type: Sequelize.ENUM("confirmed", "canceled"),
+                allowNull: false,
+            },
+            startDateTime: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            endDateTime: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            grossPrice: {
+                type: Sequelize.DECIMAL(15, 2),
+                allowNull: false,
+            },
+            netPrice: {
+                type: Sequelize.DECIMAL(15, 2),
+                allowNull: false,
+            },
+            PromoCodeName: {
+                type: Sequelize.STRING,
+                references: {
+                    model: "PromoCodes",
+                    key: "name",
+                },
+            },
+            RoomName: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                onDelete: "CASCADE",
+                references: {
+                    model: "Rooms",
+                    key: "name",
+                },
+            },
+            StudentEmail: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                references: {
+                    model: "students",
+                    key: "email",
+                },
+            },
+        });
+    },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("bookings");
-  },
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable("bookings");
+    },
 };
--- public/javascript/addRoom.js
+++ public/javascript/addRoom.js
@@ -5,67 +5,80 @@ document.getElementById("addRoom").addEventListener("submit", handleSubmits);
 document.getElementById("addRoom").addEventListener("submit", handleSubmits2);
 
 $(document).ready(() => {
-    var currentTime = new Date();
-    currentTime.setSeconds(0, 0);
-    document
-        .getElementById("launchDate")
-        .setAttribute("min", dateToLocalISOString(currentTime).replace(":00.000Z", ""))
+  var currentTime = new Date();
+  currentTime.setSeconds(0, 0);
+  document
+    .getElementById("launchDate")
+    .setAttribute(
+      "min",
+      dateToLocalISOString(currentTime).replace(":00.000Z", "")
+    );
 });
 
 function dateToLocalISOString(date) {
-    return new Date(
-        date.getTime() - date.getTimezoneOffset() * 60000
-    ).toISOString();
+  return new Date(
+    date.getTime() - date.getTimezoneOffset() * 60000
+  ).toISOString();
 }
-
-
-async function handleSubmits2(event) {
-    event.preventDefault();
-    const radio2 = document.getElementById("flexCheckDefault2").value;
-
-    const result = await axios.post("api/amenity", {
-            type: radio2,
-            RoomName: $('#Rname').val(),
-        })
-        .then(function(response) {
-            console.log(response);
-        })
-}
-
-
 
 async function handleSubmits(event) {
-    event.preventDefault();
-    const radio1 = document.getElementById("flexCheckDefault").value;
+  event.preventDefault();
+  const radio1 = document.getElementById("flexCheckDefault").value;
+  //const room1 = document.getElementById("Rname").value;
 
-    const result = await axios.post("api/amenity", {
-            type: radio1,
-            RoomName: $('#Rname').val(),
-        })
-        .then(function(response) {
-            console.log(response);
-        })
+  const result = await axios
+    .post("api/amenity", {
+      type: radio1,
+      RmName: $("#Rname").val(),
+    })
+    .then(function (response) {
+      console.log(response);
+    });
 }
 
+async function handleSubmits2(event) {
+  event.preventDefault();
+  const radio2 = document.getElementById("flexCheckDefault2").value;
+
+  const result = await axios
+    .post("api/amenity", {
+      type: radio2,
+      RoomName: $("#Rname").val(),
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+}
+
+async function handleSubmits(event) {
+  event.preventDefault();
+  const radio1 = document.getElementById("flexCheckDefault").value;
+
+  const result = await axios
+    .post("api/amenity", {
+      type: radio1,
+      RoomName: $("#Rname").val(),
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+}
 
 async function handleSubmit(event) {
-    event.preventDefault();
-    const bookingDate = document.getElementById("launchDate").value;
-    const bookDate = new Date(bookingDate);
-    console.log(bookingDate)
+  event.preventDefault();
+  const bookingDate = document.getElementById("launchDate").value;
+  const bookDate = new Date(bookingDate);
+  console.log(bookingDate);
 
-
-    const result = await axios.post("api/rooms", {
-            name: $('#Rname').val(),
-            capacity: $('#Rcapacity').val(),
-            location: $('#Rlocation').val(),
-            launchDateTime: bookDate,
-            hourlyRate: $('#Rprice').val(),
-
-
-
-        })
-        .then(function(response) {
-            console.log(response);
-        })
+  const result = await axios
+    .post("api/rooms", {
+      name: $("#Rname").val(),
+      capacity: $("#Rcapacity").val(),
+      location: $("#Rlocation").val(),
+      launchDateTime: bookDate,
+      hourlyRate: $("#Rprice").val(),
+    })
+    .then(function (response) {
+      console.log(response);
+    });
 }
